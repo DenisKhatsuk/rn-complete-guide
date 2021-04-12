67bb065f1de20a732bca460beda814e18bfa8eed
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-04-12 18:03:24 +0300

    refactor: #26 use stylesheet styles instead of inline ones

--- App.js
+++ App.js
@@ -3,26 +3,13 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 const App = () => {
   return (
-    <View style = { {paddingTop: 50, paddingHorizontal: 20} }>
-      <View style = {
-        {
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }
-      }>
-        <TextInput 
-          placeholder = "Course Goal"
-          style = { 
-            {
-              width: '80%',
-              padding: 10,
-              borderColor: 'black',
-              borderWidth: 1,
-            } 
-          }
+    <View style={ styles.container }>
+      <View style={ styles.inputContainer }>
+        <TextInput
+          placeholder="Course Goal"
+          style={ styles.input }
         />
-        <Button title = "ADD" />
+        <Button title="ADD" />
       </View>
       <View>
         <Text>Body!</Text>
@@ -32,7 +19,21 @@ const App = () => {
 }
 
 const styles = StyleSheet.create({
-  
+  container: { 
+    paddingTop: 50, 
+    paddingHorizontal: 20 
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
 });
 
 export default App;
